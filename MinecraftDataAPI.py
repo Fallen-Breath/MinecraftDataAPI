9076780ad246108c80c9f7a335f9f050529eb03e
Fallen_Breath <yqx791125833@gmail.com>
2021-03-02 22:10:28 +0800

    fixed `get_server_player_list` not works in 1.16.x servers fixed #2 v1.2.2

--- MinecraftDataAPI.py
+++ MinecraftDataAPI.py
@@ -11,7 +11,7 @@ from mcdreforged.api.all import *
 
 PLUGIN_METADATA = {
 	'id': 'minecraft_data_api',
-	'version': '1.2.1',
+	'version': '1.2.2',
 	'name': 'Minecraft Data API',
 	'description': 'A MCDReforged api plugin to get player data information and more',
 	'author': [
@@ -179,10 +179,19 @@ class ServerDataGetter:
 	def on_info(self, info: Info):
 		if not info.is_user:
 			if self.player_list.is_querying():
-				# There are 6 of a max 100 players online: 122, abc, xxx, www, QwQ, bot_tob
-				parsed = parse.parse(r'There are {amount:d} of a max {limit:d} players online:{players}', info.content)
-				if parsed is not None and parsed['players'].startswith(' '):
-					self.player_list.result_queue.put((parsed['amount'], parsed['limit'], parsed['players'][1:]))
+				formatters = (
+					# <1.16
+					# There are 6 of a max 100 players online: 122, abc, xxx, www, QwQ, bot_tob
+					r'There are {amount:d} of a max {limit:d} players online:{players}',
+					# >=1.16
+					# There are 1 of a max of 20 players online: Fallen_Breath
+					r'There are {amount:d} of a max of {limit:d} players online:{players}',
+				)
+				for formatter in formatters:
+					parsed = parse.parse(formatter, info.content)
+					if parsed is not None and parsed['players'].startswith(' '):
+						self.player_list.result_queue.put((parsed['amount'], parsed['limit'], parsed['players'][1:]))
+						break
 
 
 player_data_getter = None  # type: Optional[PlayerDataGetter]
